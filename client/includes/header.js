b37e29af4a737587988192b29bc996e95666eff7
Gareth Hayes <Gareth.Hayes@gmail.com>
2016-03-06 18:56:58 +1000

    Fix error causing degraded performance

--- client/includes/header.js
+++ client/includes/header.js
@@ -2,10 +2,4 @@ Template.header.helpers({
     displayRegister: function () {
         if (Meteor.user().profile) {return "none";}
     }
-})
-
-Template.heading.helpers({
-    heading: function () {
-        return pageTitle;
-    }
-})
+});
