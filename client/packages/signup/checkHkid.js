8c4d89d98311f06ce6ec9b6f072bca6ec3cea9a3
Gareth Hayes <Gareth.Hayes@gmail.com>
2016-03-10 08:36:32 +1000

    Add foundation for API call to validate address
    API call to als.ogcio.gov.hk (HK Government address lookup service)

--- client/packages/signup/checkHkid.js
+++ client/packages/signup/checkHkid.js
@@ -7,11 +7,18 @@ Template.checkHkid.events({
 Meteor.call('checkHKID', idcode, function(error, result) { // display the error to the user and abort
     if (error) {sAlert.error(error.reason);
     } else {
-        sAlert.info('Your HKID is valid', {});
+        // sAlert.info('Your HKID is valid', {});
     }
 })
 
-}});
+},
+
+    'blur .building': function() { Meteor.call("checkAddress", function(error, results) {
+         console.log(results.content); //results.data should be a JSON object
+       // console.log(results.AddressLookupResult.RequestAddress[0].AddressLine[0]);
+    })}
+
+});
 
 Template.checkHkid.helpers({
     ipaddress: function(){
@@ -19,23 +26,7 @@ Template.checkHkid.helpers({
             Session.set('IPaddr', result[1]);
         });
         return(Session.get('IPaddr'));
-},
+    }});
 
-    jsontest: HTTP.call( 'GET', 'http://jsonplaceholder.typicode.com/posts', {}, function( error, response ) {
-        if ( error ) {
-            return( error );
-        } else {
-            return(response.data)
-            alert( response.data() );
-            /*
-             This will return the HTTP response object that looks something like this:
-             {
-             content: "String of content...",
-             data: Array[100], <-- Our actual data lives here.
-             headers: {  Object containing HTTP response headers }
-             statusCode: 200
-             }
-             */
-        }
-    })
-    });
+
+
