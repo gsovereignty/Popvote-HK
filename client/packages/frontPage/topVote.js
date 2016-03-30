52fbc6b72e6114eb5da231a150d5599b16db066e
Gareth Hayes <Gareth.Hayes@gmail.com>
2016-03-31 02:15:06 +0800

    Create top vote layout for home page

---
+++ client/packages/frontPage/topVote.js
@@ -0,0 +1,56 @@
+/**
+ * Created by gareth on 30/3/16.
+ */
+Template.topVote.events({
+    'click .yesvote': function(e) {
+        e.preventDefault();
+        Meteor.call('voteYes', this._id, function(error, result) {if (error) {sAlert.error(error.reason)} else {sAlert.info('Your vote has been recorded', {})}});
+    },
+
+    'click .novote': function(e) {
+        e.preventDefault();
+        Meteor.call('voteNo', this._id, function(error, result) {if (error) {sAlert.error(error.reason)} else {sAlert.info('Your vote has been recorded', {})}});
+
+    },
+    'click .comments': function() {
+        Router.go('commentList', {_id: this._id});
+    },
+
+    'click .subvotes': function() {
+        Router.go('displaySubvotes', {_id: this._id});
+    }
+
+});
+
+Template.topVote.helpers({
+    officialStatus: function(){
+        if (this.isofficial === true) {
+            return "(Official)";
+        } else {}
+    },
+
+    novotes: function () {
+
+        var novotes = this.noVotes;
+        var yesvotes = this.yesVotes;
+        var total = novotes + yesvotes;
+        var pc = novotes / total * 100;
+        return Math.floor(pc);
+    },
+    yesvotes: function () {
+
+        var novotes = this.noVotes;
+        var yesvotes = this.yesVotes;
+        var total = novotes + yesvotes;
+        var pc = yesvotes / total * 100;
+        return Math.ceil(pc);
+    },
+    voteDisplay: function() {if(Meteor.user() === null){return "block"} else if(Session.get("showVoted") !== true){
+        {if(_.include(this.yesVoters, Meteor.userId())) {return "none"}
+        else {if(_.include(this.noVoters, Meteor.userId())) {return "none"} else {return "block"}}
+        }
+    }},
+
+    disabled: function() {if(Meteor.user()){return ""} else {return "disabled"}}
+
+});
